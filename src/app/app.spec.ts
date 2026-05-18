89f3d4903def82a1f18aaa97c52b1772984d1d25
elvisjsAtlas1 <chat02jselvis@gmail.com>
2026-05-18 09:26:20 -0500

    Add frontend CI workflow

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
@@ -14,10 +15,4 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-astramaco-iii');
-  });
 });
